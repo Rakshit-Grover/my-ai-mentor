a154f546b4e40180807b1863600d046ace1a12e4
Rakshit-Grover <rakshitgrover88@gmail.com>
2026-03-27 17:51:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if api_key:
         """
         
         model = genai.GenerativeModel(
-            model_name="gemini-pro",
+            model_name="gemini-1.5-flash",
             system_instruction=system_instruction
         )
 
